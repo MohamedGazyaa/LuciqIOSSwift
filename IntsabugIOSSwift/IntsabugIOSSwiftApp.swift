33d7c3027173b5e5cf9d0c621536686f671a5129
MohamedGazyaa <mohamed.gazya.13@gmail.com>
2025-07-19 17:47:11 +0300

    Initial Commit

---
+++ IntsabugIOSSwift/IntsabugIOSSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntsabugIOSSwiftApp.swift
+//  IntsabugIOSSwift
+//
+//  Created by Mohamed Gazya on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntsabugIOSSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
