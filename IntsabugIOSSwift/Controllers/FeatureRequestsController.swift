2a300842cb06884c7bd1290836f837b9ef1ed0a8
Mirette <mirettewagdy98@gmail.com>
2025-11-03 01:30:43 +0200

    Added feature Requests

--- IntsabugIOSSwift/Controllers/FeatureRequestsController.swift
+++ IntsabugIOSSwift/Controllers/FeatureRequestsController.swift
@@ -6,7 +6,46 @@
 //
 
 import Foundation
-
+import LuciqSDK
 class FeatureRequestsController: ObservableObject{
     
+    @Published var isEnabled = true
+    @Published var NewEmailFieldOption = true
+    @Published var CommentOption = true
+    
+    
+    func handleStateChange() {
+        if isEnabled {
+            FeatureRequests.enabled = true
+
+        } else {
+            FeatureRequests.enabled = false
+        }
+    }
+    
+ func showFeatureRequest(){
+    FeatureRequests.show()    }
+    
+    func applySettings(EmailFieldForNew: Bool, EmailCommentOption:Bool ){
+                if(EmailFieldForNew != self.NewEmailFieldOption){
+                            ApplyEmailFieldForNew(state: EmailFieldForNew)
+                        }
+        if(EmailCommentOption != self.CommentOption){
+                    ApplyEmailCommentOption(state: EmailCommentOption)
+                }
+     
+                }
+    func ApplyEmailFieldForNew (state: Bool){
+        FeatureRequests.setEmailFieldRequired(true, for: [.requestNewFeature])
+                        self.NewEmailFieldOption = state
+                }
+    
+    func ApplyEmailCommentOption (state: Bool){
+        FeatureRequests.setEmailFieldRequired(true, for: [.addCommentToFeature])
+                        self.CommentOption = state
+                }
+
+    
 }
+    
+    
